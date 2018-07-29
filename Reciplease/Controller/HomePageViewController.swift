d2cf5d834efaa1c0f118849de809c840c714c797
Mehdi <medev.kym@gmail.com>
2018-07-29 18:54:52 +0200

    Add tab bar & navigation controller in storyboard

---
+++ Reciplease/Controller/HomePageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomePageViewController.swift
+//  Reciplease
+//
+//  Created by Mehdi on 29/07/2018.
+//  Copyright © 2018 Mehdi. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
