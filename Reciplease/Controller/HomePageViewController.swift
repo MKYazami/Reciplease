ff54eeac59ca8948e27f6f1c448084fc642631a2
Mehdi <medev.kym@gmail.com>
2018-08-18 14:19:19 +0200

    Add ingredients functionality in HomePageViewController done

--- Reciplease/Controller/HomePageViewController.swift
+++ Reciplease/Controller/HomePageViewController.swift
@@ -9,16 +9,19 @@
 import UIKit
 
 class HomePageViewController: UIViewController {
+    // MARK: Properties
+    
     // MARK: Outlets
     @IBOutlet weak var ingredientsTextField: UITextField!
-    @IBOutlet weak var indredientsTextView: UITextView!
+    @IBOutlet weak var ingredientsTextView: UITextView!
     
     // MARK: Actions
     @IBAction func addIngredients() {
-        
+        getIngredientsFromTextField(ingredients: ingredientsTextField.text)
     }
     
     @IBAction func clearIngredients() {
+        self.ingredientsTextView.text = ""
         
     }
         
@@ -29,6 +32,49 @@ class HomePageViewController: UIViewController {
     // MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        resetTextsViews()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    private func getIngredientsFromTextField(ingredients: String?) {
+        guard let ingredients = ingredients else {
+            return
+        }
+        
+        if !ingredients.isEmpty {
+            let ingredientsInArray = ingredients.components(separatedBy: [" "])
+        
+            addIngredientsToList(ingredients: ingredientsInArray)
+        }
+    }
+    
+    private func addIngredientsToList(ingredients: [String]) {
+        // ingredientsTextView.text + "" => To avoid losing previously entered ingredients after erasing the text field
+        var ingredientToList = ingredientsTextView.text + ""
+        
+        for ingredient in ingredients {
+            if ingredient.count > 2 && !ingredient.isEmpty  {
+                ingredientToList += "﹆ \(ingredient) \n"
+            }
+        }
+        
+        ingredientsTextView.text = ingredientToList
+    }
+    
+    private func resetTextsViews() {
+        ingredientsTextField.text = ""
+        ingredientsTextView.text = ""
+    }
+    
+    private func alertMessage(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true)
     }
 
 }
