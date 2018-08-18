f9b1c6e4d07e68b432512333d1e51a8ec52c5a43
Mehdi <medev.kym@gmail.com>
2018-08-18 18:48:41 +0200

    Add clear ingredients functionality with alert message confirmation to delete or cancel in HomePage

--- Reciplease/Controller/HomePageViewController.swift
+++ Reciplease/Controller/HomePageViewController.swift
@@ -21,8 +21,9 @@ class HomePageViewController: UIViewController {
     }
     
     @IBAction func clearIngredients() {
-        self.ingredientsTextView.text = ""
-        
+        if !ingredientsTextView.text.isEmpty {
+            clearIngredientsConfirmation()
+        }
     }
         
     @IBAction func searchRecipes() {
@@ -32,7 +33,7 @@ class HomePageViewController: UIViewController {
     // MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        delegatesSetup()
+        setupDelegates()
         resetTextsViews()
     }
     
@@ -41,22 +42,32 @@ class HomePageViewController: UIViewController {
         
     }
     
+    /// Get ingredient from user entred in text field
+    ///
+    /// - Parameter ingredients: String contained in the text field
     private func getIngredientsFromTextField(ingredients: String?) {
+        
         guard let ingredients = ingredients else {
             return
         }
         
         if !ingredients.isEmpty {
+            // Transforming the ingredients in string separated by space to ingredients in array
             let ingredientsInArray = ingredients.components(separatedBy: [" "])
         
-            addIngredientsToList(ingredients: ingredientsInArray)
+            addIngredientsToTextView(ingredients: ingredientsInArray)
         }
     }
     
-    private func addIngredientsToList(ingredients: [String]) {
+    /// Add ingredients to the text view
+    ///
+    /// - Parameter ingredients: The ingredients stored in string array
+    private func addIngredientsToTextView(ingredients: [String]) {
+        
         // ingredientsTextView.text + "" => To avoid losing previously entered ingredients after erasing the text field
         var ingredientToList = ingredientsTextView.text + ""
         
+        // Fill ingredients from array to string with new line to present them as list format
         for ingredient in ingredients {
             if ingredient.count > 2 && !ingredient.isEmpty  {
                 ingredientToList += "﹆ \(ingredient) \n"
@@ -64,21 +75,39 @@ class HomePageViewController: UIViewController {
         }
         
         ingredientsTextView.text = ingredientToList
+        
+        // Dismiss keyboard after add ingredient(s)
+        if !ingredientToList.isEmpty {
+            view.endEditing(true)
+        }
     }
     
+    /// Present confirmation message to delete the ingredients from text view with the possibility to cancel the deletation
+    private func clearIngredientsConfirmation() {
+        let alertMessage = UIAlertController(title: "Delete all ingredients?", message: "If you erase the ingredients you will not be able to recover them!", preferredStyle: .alert)
+        
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { (_) in
+            self.view.endEditing(true)
+            self.ingredientsTextView.text = ""
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in
+            return
+        }
+        
+        alertMessage.addAction(deleteAction)
+        alertMessage.addAction(cancelAction)
+        
+        present(alertMessage, animated: true, completion: nil)
+    }
+    
+    /// Reset text field & text view as empty texts
     private func resetTextsViews() {
         ingredientsTextField.text = ""
         ingredientsTextView.text = ""
     }
     
-    private func alertMessage(title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true)
-    }
-    
-    private func delegatesSetup() {
+    private func setupDelegates() {
         ingredientsTextField.delegate = self
     }
 
