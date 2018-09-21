8880ba79c5ceb96088e42bc00d3d80075f836a14
Mehdi <medev.kym@gmail.com>
2018-09-21 15:00:06 +0200

    Test Recipe methods

---
+++ RecipleaseTests/ReicpeTestCase.swift
@@ -0,0 +1,92 @@
+//
+//  RecipeTestCase.swift
+//  RecipleaseTests
+//
+//  Created by Mehdi on 21/09/2018.
+//  Copyright © 2018 Mehdi. All rights reserved.
+//
+
+import XCTest
+@testable import Reciplease
+
+class RecipeTestCase: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    // MARK: convertSecondsToMinutesOrHours Tests
+    func testGivenTimeInSecondsIs60_WhenConvertToMinutes_ThenShouldReturns_1m() {
+        let seconds = 60
+        
+        let minutes = Recipe.convertSecondsToMinutesOrHours(numberOfSeconds: seconds)
+        
+        XCTAssertEqual(minutes, "1m")
+    }
+    
+    func testGivenTimeInSecondsIs3600_WhenConvertToHours_ThenShouldReturns_1h0() {
+        let seconds = 3600
+        
+        let minutes = Recipe.convertSecondsToMinutesOrHours(numberOfSeconds: seconds)
+        
+        XCTAssertEqual(minutes, "1h0")
+    }
+    
+    // MARK: defineRatingStars Tests
+    func testGivenRating1_WhenDefineRatingStars_ThenShouldReturns_rating1Of5() {
+        let rating = 1
+        
+        let imageName = Recipe.defineRatingStars(rating: rating)
+        
+        XCTAssertEqual(imageName, "rating1Of5")
+    }
+    
+    func testGivenRating2_WhenDefineRatingStars_ThenShouldReturns_rating2Of5() {
+        let rating = 2
+        
+        let imageName = Recipe.defineRatingStars(rating: rating)
+        
+        XCTAssertEqual(imageName, "rating2Of5")
+    }
+    
+    func testGivenRating3_WhenDefineRatingStars_ThenShouldReturns_rating3Of5() {
+        let rating = 3
+        
+        let imageName = Recipe.defineRatingStars(rating: rating)
+        
+        XCTAssertEqual(imageName, "rating3Of5")
+    }
+    
+    func testGivenRating4_WhenDefineRatingStars_ThenShouldReturns_rating4Of5() {
+        let rating = 4
+        
+        let imageName = Recipe.defineRatingStars(rating: rating)
+        
+        XCTAssertEqual(imageName, "rating4Of5")
+    }
+    
+    func testGivenRating5_WhenDefineRatingStars_ThenShouldReturns_rating5Of5() {
+        let rating = 5
+        
+        let imageName = Recipe.defineRatingStars(rating: rating)
+        
+        XCTAssertEqual(imageName, "rating5Of5")
+    }
+    
+    func testGivenRating6_WhenDefineRatingStars_ThenShouldReturns_noRating() {
+        let rating = 0
+        
+        let imageName = Recipe.defineRatingStars(rating: rating)
+        
+        XCTAssertEqual(imageName, "noRating")
+    }
+    
+    func testGivenRatingIsNil_WhenDefineRatingStars_ThenShouldReturns_noRating() {
+        let rating: Int? = nil
+        
+        let imageName = Recipe.defineRatingStars(rating: rating)
+        
+        XCTAssertEqual(imageName, "noRating")
+    }
+    
+}
