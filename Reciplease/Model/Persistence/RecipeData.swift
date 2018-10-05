b681435c5f748040e14b4446bc30e30017b8536a
Mehdi <medev.kym@gmail.com>
2018-10-05 11:22:41 +0200

    Add entities in xcdatamodeld & RecipeData

---
+++ Reciplease/Model/Persistence/RecipeData.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeData.swift
+//  Reciplease
+//
+//  Created by Mehdi on 05/10/2018.
+//  Copyright © 2018 Mehdi. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class RecipeData: NSManagedObject {
+    
+}
+
+class DetailedRecipeData: NSManagedObject {
+    
+}
