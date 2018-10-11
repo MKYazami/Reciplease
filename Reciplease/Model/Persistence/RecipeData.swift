2979506883fde2c139a9925a8bb681dda6afa8c3
Mehdi <medev.kym@gmail.com>
2018-10-11 15:06:05 +0200

    Add getAllRecipes in RecipeData

--- Reciplease/Model/Persistence/RecipeData.swift
+++ Reciplease/Model/Persistence/RecipeData.swift
@@ -11,6 +11,14 @@ import CoreData
 
 class RecipeData: NSManagedObject {
     
+    static var getAllRecipes: [RecipeData] {
+        let request: NSFetchRequest<RecipeData> = RecipeData.fetchRequest()
+        
+        guard let recipes = try? AppDelegate.viewContext.fetch(request) else { return [] }
+        
+        return recipes
+    }
+    
 }
 
 class DetailedRecipeData: NSManagedObject {
